6119551437dc46f1853b9bac5e64dc5719b40d08
Luke Askew <laskew@resource.com>
2014-02-01 09:10:53 -0500

    Combine "data" and "template" tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,16 +35,11 @@ module.exports = function (grunt) {
 			data: {
 				files: [
 					"<%= fabricator.src %>/{components,structures,documentation}/*.md",
-					"<%= fabricator.src %>/{components,structures,documentation}/*.html"
-				],
-				tasks: ["collate", "compile-templates"]
-			},
-			templates: {
-				files: [
+					"<%= fabricator.src %>/{components,structures,documentation}/*.html",
 					"<%= fabricator.src %>/views/**/*.html",
 					"<%= fabricator.src %>/prototypes/*.html"
 				],
-				tasks: ["compile-templates"]
+				tasks: ["collate", "compile-templates"]
 			},
 			assets: {
 				files: [
