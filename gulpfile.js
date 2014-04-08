7fb6472b75be0619e694abd7fd37614ef6199d21
Luke Askew <laskew@resource.com>
2014-04-08 13:22:22 -0400

    Clean up gulpfile.
    Closes #40

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,50 @@
-"use strict";
+'use strict';
 
-// plugins
-var gulp = require("gulp");
-var gutil = require("gulp-util");
-var plumber = require("gulp-plumber");
-var gulpif = require("gulp-if");
-var Q = require("q");
-var rename = require("gulp-rename");
-var concat = require("gulp-concat");
-var clean = require("gulp-clean");
-var sass = require("gulp-sass");
-var csso = require("gulp-csso");
-var prefix = require("gulp-autoprefixer");
-var uglify = require("gulp-uglify");
-var browserify = require("browserify");
-var source = require("vinyl-source-stream");
-var collate = require("./tasks/collate");
-var template = require("./tasks/template");
-var connect = require("gulp-connect");
-var imagemin = require("gulp-imagemin");
+// modules
+var browserify = require('browserify');
+var clean = require('gulp-clean');
+var collate = require('./tasks/collate');
+var concat = require('gulp-concat');
+var connect = require('gulp-connect');
+var csso = require('gulp-csso');
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var imagemin = require('gulp-imagemin');
+var plumber = require('gulp-plumber');
+var prefix = require('gulp-autoprefixer');
+var Q = require('q');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var source = require('vinyl-source-stream');
+var template = require('./tasks/template');
+var uglify = require('gulp-uglify');
 
 
-// env vars
-gutil.env.production = gutil.env.production || false;
+// configuration
+var config = {
+	dev: true,
+	src: {
+		scripts: {
+			fabricator: [
+				'./src/fabricator/js/prism.js',
+				'./src/fabricator/js/fabricator.js'
+			],
+			toolkit: './src/toolkit/assets/js/toolkit.js'
+		},
+		styles: {
+			fabricator: './src/fabricator/scss/fabricator.scss',
+			toolkit: './src/toolkit/assets/scss/toolkit.scss'
+		},
+		images: './src/toolkit/assets/img/**/*',
+		templates: './src/toolkit/views/*.html'
+	},
+	dest: './dist'
+};
 
 
 // clean
-gulp.task("clean", function () {
-	return gulp.src(["dist"], {
+gulp.task('clean', function () {
+	return gulp.src([config.dest], {
 		read: false
 	})
 	.pipe(clean());
@@ -35,81 +52,81 @@ gulp.task("clean", function () {
 
 
 // styles
-gulp.task("styles:fabricator", function () {
-	return gulp.src("src/fabricator/scss/**/*.scss")
+gulp.task('styles:fabricator', function () {
+	return gulp.src(config.src.styles.fabricator)
 		.pipe(plumber())
 		.pipe(sass({
 			errLogToConsole: true
 		}))
-		.pipe(prefix("last 1 version"))
-		.pipe(gulpif(gutil.env.production, csso()))
-		.pipe(rename("f.css"))
-		.pipe(gulp.dest("dist/assets/css"))
-		.pipe(gulpif(!gutil.env.production, connect.reload()));
+		.pipe(prefix('last 1 version'))
+		.pipe(gulpif(!config.dev, csso()))
+		.pipe(rename('f.css'))
+		.pipe(gulp.dest(config.dest + '/assets/css'))
+		.pipe(gulpif(config.dev, connect.reload()));
 });
 
-gulp.task("styles:toolkit", function () {
-	return gulp.src("src/toolkit/assets/scss/**/*.scss")
+gulp.task('styles:toolkit', function () {
+	return gulp.src(config.src.styles.toolkit)
 		.pipe(plumber())
 		.pipe(sass({
 			errLogToConsole: true
 		}))
-		.pipe(prefix("last 1 version"))
-		.pipe(gulpif(gutil.env.production, csso()))
-		.pipe(gulp.dest("dist/toolkit/css"))
-		.pipe(gulpif(!gutil.env.production, connect.reload()));
+		.pipe(prefix('last 1 version'))
+		.pipe(gulpif(!config.dev, csso()))
+		.pipe(gulp.dest(config.dest + '/toolkit/css'))
+		.pipe(gulpif(config.dev, connect.reload()));
 });
 
-gulp.task("styles", ["styles:fabricator", "styles:toolkit"]);
+gulp.task('styles', ['styles:fabricator', 'styles:toolkit']);
 
 
 // scripts
-gulp.task("scripts:fabricator", function () {
-	return gulp.src(["src/fabricator/js/prism.js", "src/fabricator/js/fabricator.js"])
+gulp.task('scripts:fabricator', function () {
+	return gulp.src(config.src.scripts.fabricator)
 		.pipe(plumber())
-		.pipe(concat("f.js"))
-		.pipe(gulpif(gutil.env.production, uglify()))
-		.pipe(gulp.dest("dist/assets/js"))
-		.pipe(gulpif(!gutil.env.production, connect.reload()));
+		.pipe(concat('f.js'))
+		.pipe(gulpif(!config.dev, uglify()))
+		.pipe(gulp.dest(config.dest + '/assets/js'))
+		.pipe(gulpif(config.dev, connect.reload()));
 });
 
-gulp.task("scripts:toolkit", function () {
-	return browserify("./src/toolkit/assets/js/toolkit.js").bundle()
-		.pipe(source("toolkit.js"))
+gulp.task('scripts:toolkit', function () {
+	return browserify(config.src.scripts.toolkit).bundle()
+		.pipe(source('toolkit.js'))
 		.pipe(plumber())
-		.pipe(gulpif(gutil.env.production, uglify()))
-		.pipe(gulp.dest("dist/toolkit/js"))
-		.pipe(gulpif(!gutil.env.production, connect.reload()));
+		.pipe(gulpif(!config.dev, uglify()))
+		.pipe(gulp.dest(config.dest + '/toolkit/js'))
+		.pipe(gulpif(config.dev, connect.reload()));
 });
 
-gulp.task("scripts", ["scripts:fabricator", "scripts:toolkit"]);
+gulp.task('scripts', ['scripts:fabricator', 'scripts:toolkit']);
 
 
 // images
-gulp.task("images", function () {
-	return gulp.src("src/toolkit/assets/img/**/*")
+gulp.task('images', function () {
+	return gulp.src(config.src.images)
 		.pipe(imagemin())
-		.pipe(gulp.dest("dist/toolkit/img")
-		.pipe(gulpif(!gutil.env.production, connect.reload())));
+		.pipe(gulp.dest(config.dest + '/toolkit/img'))
+		.pipe(gulpif(config.dev, connect.reload()));
 });
 
 
 // collate
-gulp.task("collate", function () {
+gulp.task('collate', function () {
 
-	// "collate" is a little different -
+	// 'collate' is a little different -
 	// it returns a promise instead of a stream
 
 	var deferred = Q.defer();
 
 	var opts = {
 		materials: [
-			"components",
-			"structures",
-			"prototypes",
-			"documentation"
+			'components',
+			'structures',
+			'prototypes',
+			'documentation'
 		],
-		dest: "dist/assets/json/data.json"
+		dest: config.dest + '/assets/json/data.json'
 	};
 
 	collate(opts, deferred.resolve);
@@ -119,47 +136,49 @@ gulp.task("collate", function () {
 });
 
 // templates
-gulp.task("template", ["collate"], function () {
-	return gulp.src("src/toolkit/views/*.html")
+gulp.task('template', ['collate'], function () {
+	return gulp.src(config.src.templates)
 		.pipe(template())
-		.pipe(gulp.dest("dist"))
-		.pipe(gulpif(!gutil.env.production, connect.reload()));
-});
-
-
-// server
-gulp.task("connect", connect.server({
-	root: ["dist"],
-	port: 9000,
-	livereload: gutil.env.production ? false : {port:(Math.floor(Math.random() * (35729 - 35720 + 1) + 35720))},
-	open: {
-		file: ""
-	}
-}));
-
-// watch
-gulp.task("watch", ["connect"], function () {
-
-	gulp.watch("src/toolkit/{components,structures,prototypes,documentation,views}/*.{html,md}", ["template"]);
-	gulp.watch("src/fabricator/scss/**/*.scss", ["styles:fabricator"]);
-	gulp.watch("src/toolkit/assets/scss/**/*.scss", ["styles:toolkit"]);
-	gulp.watch("src/fabricator/js/**/*.js", ["scripts:fabricator"]);
-	gulp.watch("src/toolkit/assets/js/**/*.js", ["scripts:toolkit"]);
-
+		.pipe(gulp.dest(config.dest))
+		.pipe(gulpif(config.dev, connect.reload()));
 });
 
 
 // build
-gulp.task("build", ["clean"], function () {
-	gulp.start("styles", "scripts", "images", "template");
+gulp.task('build', ['clean'], function () {
+	gulp.start('styles', 'scripts', 'images', 'template');
 });
 
 
-// dev
-gulp.task("dev", ["build"], function () {
-	gulp.start("watch");
+// server
+gulp.task('connect', connect.server({
+	root: [config.dest],
+	port: 9000,
+	livereload: config.dev ? { port:(Math.floor(Math.random() * (35729 - 35720 + 1) + 35720)) } : false,
+	open: {
+		file: ''
+	}
+}));
+
+
+// watch
+gulp.task('watch', ['connect'], function () {
+	gulp.watch('./src/toolkit/{components,structures,prototypes,documentation,views}/*.{html,md}', ['template']);
+	gulp.watch('./src/fabricator/scss/**/*.scss', ['styles:fabricator']);
+	gulp.watch('./src/toolkit/assets/scss/**/*.scss', ['styles:toolkit']);
+	gulp.watch('./src/fabricator/js/**/*.js', ['scripts:fabricator']);
+	gulp.watch('./src/toolkit/assets/js/**/*.js', ['scripts:toolkit']);
 });
 
 
-// release build
-gulp.task("default", ["build"]);
+// development environment
+gulp.task('dev', ['build'], function () {
+	gulp.start('watch');
+});
+
+
+// default build task
+gulp.task('default', function () {
+	config.dev = false;
+	gulp.start('build');
+});
