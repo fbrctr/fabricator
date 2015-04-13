a909ea2649cb0c725135cd46dccb1cd6211e60c9
Adam Putinski <adam.putinski@gmail.com>
2015-04-13 14:47:05 -0400

    Removed config.src.vendor

--- webpack.config.js
+++ webpack.config.js
@@ -18,16 +18,6 @@ module.exports = function(fabricatorConfig) {
 		cache: {}
 	};
 
-	// If "vendor" scripts are found, assume they aren't commonjs compatible
-	// and just load them as normal scripts
-	// See: https://github.com/webpack/docs/wiki/shimming-modules#script-loader
-	var vendor = fabricatorConfig.src.scripts.vendor;
-	if (vendor) {
-		config.entry['toolkit/scripts/vendor'] = vendor.map(function(src) {
-			return 'script!' + src;
-		});
-	}
-
 	if (!fabricatorConfig.dev) {
 		config.plugins.push(
 			new webpack.optimize.UglifyJsPlugin()
